feb6e7a851e53271ad1795b37fbb2e5300f8def6
yjqiang <yjqiang1@gmail.com>
2019-07-15 06:58:48 +0800

    poster输出排序，方便查看
    dict没卵用。。。list就行吧 而且老版本的dict不是按顺序的

--- server/poster.py
+++ server/poster.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from operator import itemgetter
 
 import attr
 
@@ -11,19 +12,21 @@ class Poster:  # 推送人是一个poster
     user_latest_post_time = attr.ib(validator=attr.validators.instance_of(int))
 
 
-class PostOffice:  # 管理poster的状态
+class PostOffice:  # 管理posters的状态
     def __init__(self):
         self._posters: Dict[str, Poster] = {}
 
-    def update(self, name: str):  # 有该用户更新时间；如果没有该用户，就create新的
+    def update(self, name: str) -> None:  # 有该用户更新时间；如果没有该用户，就create新的
         if name in self._posters:
             self._posters[name].user_latest_post_time = curr_time()
         else:
             poster = Poster(user_name=name, user_latest_post_time=curr_time())
             self._posters[name] = poster
 
-    def count(self):
+    def count(self) -> list:
+        int_curr_time = curr_time()
         # 过滤删除过于久远的
         self._posters = {key: poster for key, poster in self._posters.items()
-                         if poster.user_latest_post_time + 48*3600 > curr_time()}
-        return {key: poster.user_latest_post_time for key, poster in self._posters.items()}
+                         if poster.user_latest_post_time + 12*3600 > int_curr_time}
+        return [(key, poster.user_latest_post_time) for key, poster in
+                sorted(self._posters.items(), key=itemgetter(0))]
